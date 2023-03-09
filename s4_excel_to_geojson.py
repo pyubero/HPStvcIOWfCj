e5709e4b941918a587f6886f9093d07f2bd2d0cf
Pablo Yubero <pabloyub@hotmail.com>
2023-03-09 17:55:37 +0100

    Red circle markers in map

--- s4_excel_to_geojson.py
+++ s4_excel_to_geojson.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 import geopandas as gpd
-from tqdm import tqdm 
+from tqdm import tqdm
 
 DIR_OUTPUT = "./callejeros"
 
@@ -16,34 +16,33 @@ _filepath = os.path.join(DIR_OUTPUT, _comunidad)
 
 # ... load data
 df = pd.read_excel(f"{_filepath}_streets.xlsx")
-dispdf =df[df["Display"]==1]
+dispdf = df[df["Display"] == 1]
 municipios = dispdf["Municipality"].unique()
 
 # For every MUNICIPALITY...
 for municipio in tqdm(municipios):
-    
+
     # ... load municipality data
-    _gjson_fp = os.path.join(_filepath, f"{_comunidad}_{municipio}.geojson") 
+    _gjson_fp = os.path.join(_filepath, f"{_comunidad}_{municipio}.geojson")
     _gjson = gpd.read_file(_gjson_fp)
-    
+
     # ... list streets to be displayed on the map
     _mun_df = dispdf[dispdf["Municipality"] == municipio]
     _streets = _mun_df["Street_name"].to_list()
-    
+
     # For every STREET to de displayed...
     for street in _streets:
-        geometry = _gjson[_gjson["Name"]==street]["geometry"].iloc[0]
+        geometry = _gjson[_gjson["Name"] == street]["geometry"].iloc[0]
 
         # ... append to output
         MUNICIPALITIES.append(municipio)
         STREET_NAMES.append(street)
         GEOMETRIES.append(geometry)
-    
+
 # Save GeoJSON
 out_df = pd.DataFrame(
-            zip(MUNICIPALITIES,STREET_NAMES),
-            columns=["Municipality","Name"]
+            zip(MUNICIPALITIES, STREET_NAMES),
+            columns=["Municipality", "Name"]
             )
 out_df = gpd.GeoDataFrame(out_df, geometry=GEOMETRIES)
 out_df.to_file("mappable.geojson", driver="GeoJSON")
-
